0df673768e026afb6909949b24888bdb45b3e5d9
Justin Sepvian <zhanjustin5@gmail.com>
2026-09-23 10:53:09 +0700

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.go
@@ -0,0 +1,28 @@
+func threeSum(nums []int) [][]int {
+	sort.Slice(nums, func(i, j int) bool {return nums[i] < nums[j]})
+
+	ans := make([][]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		j := i + 1
+		k := len(nums) - 1
+		if i > 0 && nums[i] == nums[i - 1] {
+			continue
+		}
+
+		for j < k {
+			if nums[j] + nums[k] + nums[i] == 0 {
+				ans = append(ans,[]int{nums[i], nums[j], nums[k]})	
+				j++
+				k--
+				for j < k && nums[j] == nums[j - 1]  {j++}
+				for j < k && nums[k] == nums[k + 1] {k--}
+			} else if nums[j] + nums[k] + nums[i] > 0 {
+				k--
+			} else {
+				j++
+			}
+		}
+	}
+	return ans
+}
